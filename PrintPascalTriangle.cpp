39be8e5281968bbfce969124fdb596259ba4f41e
Kartik Kankurte <kartikkankurte@gmail.com>
2023-06-04 18:50:30 +0530

    completed printPascalsTriangle

---
+++ PrintPascalTriangle.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+vector<long long> generateRow(int row) {
+    long long ans = 1;
+    vector<long long> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
+
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+vector<vector<long long int>> printPascal(int n)  {
+    vector<vector<long long>> ans;
+    //store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;
+}
