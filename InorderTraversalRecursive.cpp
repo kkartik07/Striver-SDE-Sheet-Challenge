044c8120a3a97b2ec00e26f40d078d4c86c85037
Kartik Kankurte <kartikkankurte@gmail.com>
2023-06-14 17:08:57 +0530

    completed recursive Inorder traversal

---
+++ InorderTraversalRecursive.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void help(TreeNode* node,vector<int> &ans){
+    if(node==NULL)return;
+    help(node->left,ans);
+    ans.push_back(node->data);
+    help(node->right,ans);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{   
+    vector<int> ans;
+    help(root,ans);
+    return ans;
+}
