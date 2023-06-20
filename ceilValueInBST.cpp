039d1124fc295c6ec76bcfba8b95cad98f6aa72a
Kartik Kankurte <kartikkankurte@gmail.com>
2023-06-20 15:56:51 +0530

    completed find ceil value in BST

---
+++ ceilValueInBST.cpp
@@ -0,0 +1,14 @@
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    int ceil=-1;
+    while(node!=NULL){
+        if(node->data==x)return x;
+        else if(node->data>x){
+            ceil=node->data;
+            node=node->left;
+        }
+        else node=node->right;
+    }
+    return ceil;
+}
